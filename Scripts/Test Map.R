8050bb7717df5cbb978521685970caf7071b3059
WimKo <wim.konings@reacfin.com>
2017-01-29 20:11:27 +0100

    Delet Old stuff

--- Scripts/Test Map.R
+++
@@ -1,8 +0,0 @@
-require(rgdal)
-
-path=paste(getwd(), "/Input/Mapbox/features.json", sep="")
-Polygons = readOGR(path, "OGRGeoJSON",  require_geomType="wkbPolygon")
-Roads = readOGR(path, "OGRGeoJSON",  require_geomType="wkbLineString")
-
-plot(Roads, col="red")
-plot(Polygons, add=TRUE)
